2e0ce0c498a36b92fde2d5f74631f452430ce426
Baptiste <tite7001@gmail.com>
2023-01-02 16:29:53 +0100

    chgt (1,i) en (0,i)

--- main.cpp
+++ main.cpp
@@ -10,10 +10,10 @@ int main(){
 
     Complete_put put_c = Complete_put(1.0,300.0,1000,1000);
     put_c.Solution(100.0,0.1,0.1);
-    /*for(int i = 0 ; i < 100 ; i++)
+    for(int i = 0 ; i < 100 ; i++)
     {
         std::cout<<put_c(0,i)<<std::endl;
-    }*/
+    }
 
     std::cout<<"/////////////////////////////"<<std::endl;
 
@@ -48,7 +48,7 @@ int main(){
 
     // Courbe exemple pour Graph
 
-    Graph graph(640, 480, "Ma courbe");
+    /*Graph graph(640, 480, "Ma courbe");
 
     const int n = 100;
     float values[n][2];
@@ -60,10 +60,11 @@ int main(){
         graph.addPoint(values[i][0], values[i][1]);
     }
 
-    graph.draw();
+    graph.draw(); */
 
     /*------------------------------------------------------------------------*/
     // Création de l'objet graphique
+    
     Graph graph1(640, 480, "put_c");
     Graph graph2(640, 480, "call_c");
     Graph graph3(640, 480, "put_r");
@@ -71,19 +72,19 @@ int main(){
 
     // Ajout des valeurs au graphique
     for (int i=0;i<40;i++){
-        graph1.addPoint(i, put_c(1,i));
-        graph2.addPoint(i, call_c(1,i));
-        graph3.addPoint(i, put_r(1,i));
-        graph4.addPoint(i, call_r(1,i));
+        graph1.addPoint(i, put_c(0,i));
+        graph2.addPoint(i, call_c(0,i));
+        graph3.addPoint(i, put_r(0,i));
+        graph4.addPoint(i, call_r(0,i));
     }
 
     // Affichage du graphique
     graph1.draw();
     graph2.draw();
     graph3.draw();
-    graph4.draw();
+    graph4.draw();  
 
-    // Attente de fermerture de la fenêtre
+    
     bool running = true;
     while (running) {
         SDL_Event event;
